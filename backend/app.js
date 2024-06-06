fdebde38568f082c040994f5337200749ed70e2e
Hammad-Ali-3656 <hali.bscs22seecs@seecs.edu.pk>
2024-06-06 18:56:37 +0500

    Vercel URL update final

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    origin: ['*'],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
