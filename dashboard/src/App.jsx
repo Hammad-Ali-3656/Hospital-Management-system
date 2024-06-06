17e40134322a5029da3f0f8cdd986a435aac7b5c
Hammad-Ali-3656 <hali.bscs22seecs@seecs.edu.pk>
2024-06-06 17:54:24 +0500

    Vercel URL update

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          process.env.REACT_APP_BACKEND_URL + "/api/v1/user/admin/me",
+          process.env.REACT_APP_BACKEND_URL + "api/v1/user/admin/me",
           {
             withCredentials: true,
           }
