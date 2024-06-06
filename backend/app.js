c1ae2c3c7da3cf54dc91f6f9c2407a228ae5db66
Hammad-Ali-3656 <hali.bscs22seecs@seecs.edu.pk>
2024-06-06 19:18:47 +0500

    Vercel URL update finalbv

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,16 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    process.env.FRONTEND_URL_ONE
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
